1ac0af2e7207752d323b739cb6d2bb60751571de
KushalSatrasala <KushalSatrasala@users.noreply.github.com>
2022-03-23 11:59:43 +0530

    Time: 274 ms (5.07%), Space: 14 MB (26.49%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        
+        dp_map = [False for _ in range( n+1 )]
+        dp_map[1] = False
+        i = 2
+        while i <= n:
+            j = 1
+            while j < i:
+                if i % j == 0 and not dp_map[i - j]:
+                    dp_map[i] = True
+                    break
+                j += 1
+            i += 1
+        
+        print(dp_map)
+        return dp_map[n]
+        
