f4fdcf2f30769d7558da6814ec86eade7b4b8def
KushalSatrasala <KushalSatrasala@users.noreply.github.com>
2022-06-10 06:16:40 +0530

    Time: 189 ms (62.33%), Space: 14.9 MB (60.61%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,37 +1,27 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        res_add = 0
-        s_len = len(s)
-        
-        i = 0
-        ob = 0
-        cb = 0
-        while i < s_len:
-            if s[i] == "(":
-                ob += 1
-                i += 1
-            elif s[i] == ")":
-                cb = 0
-                while i < s_len and s[i] == ")":
-                    cb += 1
-                    i += 1
-                print(ob, cb)
-                if cb % 2 == 1:
-                    res_add += 1
-                    cb += 1
-                cb = cb // 2
-                if cb == ob:
-                    cb = 0
-                    ob = 0
-                    continue
-                elif cb > ob:
-                    res_add += cb - ob
-                    ob = 0
-                    cb = 0
+        count = 0
+        s = s.replace('))', '}')
+        open_bracket_count = 0
+
+        for c in s:
+            if c == '(':
+                open_bracket_count += 1
+                
+            else:
+			
+                # For ) you need to add 1 char to get ))
+                if c == ')': 
+                    count += 1
+
+                # Matching ( for ) or ))
+                if open_bracket_count:
+                    open_bracket_count -= 1
+
+
+                # No Matching ( for ) or ))
+                # Need to insert ( to balance string
                 else:
-                    ob = ob - cb
-                    cb = 0
-        
-        res_add += ob * 2
-        res_add += cb // 2
-        return res_add
+                    count += 1
+
+        return count + open_bracket_count * 2
